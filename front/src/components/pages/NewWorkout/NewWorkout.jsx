691b6971d78edb730d87f041ac58fa0e83663575
Павел <kurinkov.ps@gmail.com>
2023-01-10 11:39:16 +0300

    Feat 15: Created added func workout to db

--- front/src/components/pages/NewWorkout/NewWorkout.jsx
+++ front/src/components/pages/NewWorkout/NewWorkout.jsx
@@ -5,18 +5,58 @@ import bgImage from '../../../images/new-workout.jpg';
 import Field from '../../ui/Field/Field';
 import Button from '../../ui/Button/Button';
 import { Link } from 'react-router-dom';
+import { useMutation, useQuery } from 'react-query';
+import { $api } from '../../../api/api';
+import Alert from '../../ui/Alert/Alert';
+import Loader from '../../ui/Loader';
 
 const NewWorkout = () => {
     const [name, setName]= React.useState('');
-    const [exercises, setExercises] = React.useState('');
+    const [exercisesCurrent, setExercisesCurrent] = React.useState([]);
 
-    const handleSubmit = () => {
-        console.log('submit');
+    const {data, isSuccess} = useQuery('list exercises', ()=>
+    $api({
+        url: '/exercises',
+    }),
+    {
+        refetchOnWindowFocus: false,
+    }
+);
+
+const {
+    mutate, 
+    isLoading, 
+    isSuccess: isSuccessMutate,
+    error
+}=useMutation('Create new workout', ({exIds})=>
+$api({
+    url: '/workouts',
+    type: 'POST',
+    body: {name, exerciseIds: exIds},
+}), {
+    onSuccess(){
+        setName('');
+        setExercisesCurrent([]);
+    }
+}
+);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const exIds = exercisesCurrent.map(ex => ex.value);
+
+        mutate({
+            exIds,
+        });
     };
     return (
         <>
         <Layout bgImage={bgImage} heading='Craete new workout'/> 
             <div className='wrapper-inner-page'>
+                {error && <Alert type='error' text={error}/>}
+                {isSuccessMutate && <Alert text='Workouts created'/>}
+                {isLoading && <Loader/>}
                 <form onSubmit={handleSubmit}>
                     <Field
                     placeholder = 'Enter name'
@@ -24,19 +64,21 @@ const NewWorkout = () => {
                     onChange = {e => setName(e.target.value)}
                     required/>
                     <Link to='/new-exercise' className='dark-link'>Add new exercise</Link>
+                    {isSuccess && data && (
                     <ReactSelect
                         classNamePrefix='select2-selection'
                         placeholder='Exercises...'
                         title='Exercises'
-                        options={[
-                            {value: 'dklfdslkfj', label: 'Push-ups'},
-                            {value: 'ohhiywe', label: 'Pull-ups'},
-                        ]}
-                        isSearchable={true}
-                        value={exercises}
-                        onChange={setExercises}
+                        options={
+                            data.map(ex=>({
+                                value: ex._id,
+                                label: ex.name,
+                            }))}
+                        //isSearchable={true}
+                        value={exercisesCurrent}
+                        onChange={setExercisesCurrent}
                         isMulti={true}
-                    />
+                    />)}
                     <Button text = 'Create' callback={ ()=>{} }></Button>
                 </form>
             </div>
